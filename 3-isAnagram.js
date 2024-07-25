583b68c921b8a18af8651f45df43f7979f138359
Abdul Hafeez <hafeezrahimoon531@gmail.com>
2024-07-25 17:41:23 +0500

    Optimized approach to check whether a number is Anagram or not

--- 3-isAnagram.js
+++ 3-isAnagram.js
@@ -4,6 +4,7 @@
 
 // Input: (i = "silent"), (j = "listen" )  => Output: true
 
+//Ist Approach
 const isAnagram = (i, j) => {
   if (i.length !== j.length) return false;
   i = i.split("").sort().join("");
@@ -14,4 +15,22 @@ const isAnagram = (i, j) => {
 // "listen" => ["l", "i", "s", "t", "e", "n"] => ["e", "i", "l", "n", "s", "t"] => "eilnst"
 // "silent" => ["s", "i", "l", "e" ,"n", "t"] => ["e", "i", "l", "n", "s", "t"] => "eilnst"
 
+//2nd Approach
+const isAnagram2 = (i, j) => {
+  if (i.length !== j.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+  for (let index = 0; index < j.length; index++) {
+    obj1[i[index]] = (obj1[i[index]] || 0) + 1;
+    obj2[i[index]] = (obj2[i[index]] || 0) + 1;
+  }
+
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+};
+
 console.log(isAnagram("silent", "listen"));
+console.log(isAnagram2("silent", "listen"));
